c6de80aa5d48dfb98f106358afee1a66b718d219
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2022-07-28 21:01:02 -0300

    first commit

---
+++ src/Helper/EntityManagerCreator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura\Doctrine\Helper;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+class EntityManagerCreator
+{
+    public static function createEntityManager(): EntityManager
+    {
+        $isDevMode = true;
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            [__DIR__."/src"],
+            $isDevMode
+        );
+
+        $conn = [
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/../../db.sqlite',
+        ];
+
+        return EntityManager::create($conn, $config);
+    }
+}

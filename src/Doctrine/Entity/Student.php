8a47a009812431f45e7cc41349cca655786f83ee
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2022-07-28 21:37:25 -0300

    Aula 02 - Mapeando uma entidade

---
+++ src/Doctrine/Entity/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\GeneratedValue;
+
+#[Entity]
+class Student
+{
+	#[Id]
+	#[GeneratedValue]
+	#[Column]
+	public readonly int $id
+
+    public function __construct (
+		#[Column]
+    	public readonly string $name
+	) {
+
+	}
+}
